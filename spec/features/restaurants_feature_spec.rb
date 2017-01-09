deb0f4a15aa73b95b853b5373d9f9c90fb3aa6c7
pflarcology <rhrg500@gmail.com>
2017-01-09 12:32:02 +0000

    creates restaurant model

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurant have been added' do
+    before do
+      Restaurant.create(name: 'Subway')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('Subway')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
 end
