3ebee736c2571cf0c3201dab7333644f924ddf8d
pflarcology <rhrg500@gmail.com>
2017-01-09 15:48:45 +0000

    can create restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -20,4 +20,15 @@ feature 'restaurants' do
       expect(page).not_to have_content('No restaurants yet')
     end
   end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, the displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Subway'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'Subway'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
 end
