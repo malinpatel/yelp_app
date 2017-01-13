aeb1efebf48bfdb518ba73d04a7912d346fe20c7
malin patel <malin.patel@icloud.com>
2017-01-13 10:56:03 +0000

    AJAX added to update endorsements in real time

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -9,7 +9,7 @@ feature 'endorsing reviews' do
 
   it 'a user can endorse a review, which updates the review endorsement count', js: true do
     visit '/restaurants'
-    click_link 'Endorse'
+    click_link 'Endorse Review'
     expect(page).to have_content('1 endorsement')
   end
 end
